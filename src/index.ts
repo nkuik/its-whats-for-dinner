8423897d401d1585d68636c49eb71c00f0bd2aec
Nathan Kuik <nathan.kuik@lego.com>
2023-03-04 08:26:29 +0100

    Remove all spoonacular stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Fall, Season, Spring, Summer, Winter } from "./types/ingredients";
-import { ComplexRecipeSearchParams } from "./types/spoonacular";
 
 export const findSeason = async (dateObj: Date): Promise<Season> => {
   if (Spring.firstDay <= dateObj && dateObj < Summer.firstDay) {
@@ -11,20 +10,3 @@ export const findSeason = async (dateObj: Date): Promise<Season> => {
   }
   return Winter;
 };
-
-export const transformParamsSpoonacular = async (
-  params: ReadonlyArray<string>,
-  operator: "AND" | "OR",
-): Promise<string> => {
-  if (!params.length) {
-    throw new Error("No params provided");
-  }
-  if (operator === "AND") {
-    return params.join(",");
-  }
-  return params.join("|");
-};
-
-export const makeComplexRecipeSearch = async (
-  params: ComplexRecipeSearchParams,
-): Promise<ComplexRecipeSearchResponse> => {};
